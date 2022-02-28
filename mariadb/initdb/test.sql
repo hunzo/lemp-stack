8b83cf46218a6dfb4111c344951a04e11e2c05d2
hunzo <takraw_s@hotmail.com>
2022-02-28 04:39:45 +0000

    test init db

---
+++ mariadb/initdb/test.sql
@@ -0,0 +1,20 @@
+
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: db
+-- Generation Time: Aug 05, 2019 at 03:55 PM
+-- Server version: 5.7.27
+-- PHP Version: 7.2.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
